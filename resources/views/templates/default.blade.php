06134ea8f211460cdfe50d18132efa564bd22b4d
Julien MOUNIANMAN <jmounianman@imie.lan>
2017-08-29 17:03:36 +0200

    df

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -1,16 +1,26 @@
 <html>
 <head>
     <title>Blog321 - @yield('title')</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.1/css/bulma.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.1/css/bulma.min.css"/>
 
 </head>
 <body>
 
 <div class="container">
     @component('elements.navbar')
-        @endcomponent
+    @endcomponent
+
+
+    @if (Session::has('message'))
+        <div class="message">
+            <div class="message-header"> Information </div>
+            <div class="message-body">
+                        {{session('message')}}
+            </div>
+        </div>
+    @endif
     <h1 class="title"> @yield('title')</h1>
-@yield('content')
+    @yield('content')
 </div>
 </body>
 
